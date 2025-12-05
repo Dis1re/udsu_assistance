beeb49e269d6492bec6b579fe93b674e5828dae4
SeREnbKa <eskartem@gmail.com>
2025-12-06 00:27:02 +0400

    Bebra

--- src/bot/main.py
+++ src/bot/main.py
@@ -1,8 +1,9 @@
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 import re
+from token import VKBOT_TOKEN 
 
-TOKEN = "vk1.a.enfIkdHF4ATCZye1G1Ds9T6ESVIhFNdJPVQzWTveU4niXPFfltT3JtnidWvE39znNGVcIpaR1PR3JBj8W0X9HoJ5cKpW2Ok6vFGBsG0bLSWU-SiI2sOS5bOLVe5HhxPWVhHxG6V6uokt_LVRkI0iE7nG3h_8VnzQ8MM1OPjl8gveXMQVf0ALLQMapBY3zWP3ziI9juAZrT6cMtD-QLSYkw"
+TOKEN = VKBOT_TOKEN
 
 vk_session = vk_api.VkApi(token=TOKEN)
 vk = vk_session.get_api()
