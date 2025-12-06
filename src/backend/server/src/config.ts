9430f2ea7032222f659ab471f56c9bcf804b8996
Sulfide <msulfide@mail.ru>
2025-12-07 00:41:04 +0300

    Add DB service

--- src/backend/server/src/config.ts
+++ src/backend/server/src/config.ts
@@ -1,6 +1,14 @@
 const CONFIG = {
     HOST: 'localhost',
-    PORT: 3000
+    PORT: 3000,
+
+    DB_CONFIG: {
+        user: 'postgres',
+        host: 'localhost',
+        database: 'udsu_assistance_db',
+        password: '111',
+        port: 5432
+    }
 }
 
 export default CONFIG;
